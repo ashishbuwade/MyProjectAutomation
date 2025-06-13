7a3cd4f1258fc992fd067de95c4c2d54c89734b9
ashishbuwade <ashishbuwade31@gmail.com>
2025-06-14 02:14:50 +0530

    Called test method

--- methods/Home.java
+++ methods/Home.java
@@ -3,11 +3,15 @@ package methods;
 public class Home{
 
     public static void main(String ab[]){
+        Home h = new Home();
+        h.test();
         System.out.println("Hello World");
     }
 
     public void test(){
         System.out.println("Test Method");
     }
+
+
 
 }
